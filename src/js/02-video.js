ef54df31621fe1da44baf77b7ffbfcfe6466f07c
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-07-02 23:03:24 +0300

    Done Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import _ from 'lodash';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('timeupdate', _.throttle(onPlay, 1000));
+onCurrentTimeSave();
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+function onCurrentTimeSave() {
+  const time = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(time).then(function (seconds) {
+    return;
+  });
+}
